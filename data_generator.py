b1c68c739bb723110bda53e4bf99bbc8671f3768
bhuvidange <bhuvi.a.dange16@gmail.com>
2026-03-22 22:30:30 +0530

    updated data generator

--- data_generator.py
+++ data_generator.py
@@ -1,18 +1,19 @@
-import time
-import random
+SIGNAL_STATE_MAP = {
+    "NS_GREEN": "GREEN",
+    "EW_GREEN": "GREEN",
+    "ALL_RED":  "RED",
+    "YELLOW":   "YELLOW",
+}
 
-def generate_data():
+def generate_data(sumo_record):
     return {
-        "junction_id": random.choice([
-            "SilkBoard",
-            "KR_Puram",
-            "BTM_Layout",
-            "Marathahalli",
-            "Whitefield"
-        ]),
-        "timestamp": int(time.time()),
-        "vehicle_count": random.randint(0, 30),
-        "queue_length": random.randint(0, 15),
-        "waiting_time": random.randint(0, 60),
-        "signal_state": random.choice(["RED", "GREEN", "YELLOW"])
+        "timestamp":       sumo_record["timestamp"],
+        "simulation_time": sumo_record["simulation_time"],
+        "vehicle_count":   sumo_record["vehicle_count"],
+        "north_queue":     sumo_record["north_queue"],
+        "south_queue":     sumo_record["south_queue"],
+        "east_queue":      sumo_record["east_queue"],
+        "west_queue":      sumo_record["west_queue"],
+        "waiting_time":    sumo_record["waiting_time"],
+        "signal_state":    SIGNAL_STATE_MAP.get(sumo_record["signal_state"], "RED"),
     }
